f9b83486d600152f0d9641b04b856fa1cebf2d54
Hampus <hampus.a02@gmail.com>
2020-03-03 19:42:36 +0100

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './assets/stylesheets/sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <aside className="sidebar">
+            Sidebar
+        </aside>
+    )
+}
+
+export default Sidebar;
